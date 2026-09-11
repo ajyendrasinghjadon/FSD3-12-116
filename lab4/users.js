e5acbc92d37270c754e94a4a90a5af8d2562b4fe
ajyendrasinghjadon <ajyendrasinghjadonnn@gmail.com>
2026-09-11 15:46:18 +0530

    11th September

---
+++ lab4/users.js
@@ -0,0 +1,9 @@
+//  we use in memory database
+let users = [
+    {id: 1, name: "Amit Sharma", mob: "684684xxxx", email: "amit.example@gmail.com"},
+    {id: 2, name: "Shiva Yadav", mob: "954684xxxx", email: "shiva.example@gmail.com"}
+];
+
+let nextId = 3;
+
+export const getUsers = () => users;
