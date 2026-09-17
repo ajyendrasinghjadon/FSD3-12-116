becb97e5ee58c7f2c5d6df9e20295f924f527caf
ajyendrasinghjadon <ajyendrasinghjadonnn@gmail.com>
2026-09-17 12:12:30 +0530

    17th September

--- lab4/prg7.js
+++ lab4/prg7.js
@@ -1,10 +1,34 @@
 import http from "http";
-import {getUsers} from "./users.js"
+import {addUser, getUsers} from "./users.js"
 const server = http.createServer((req, res) => {
-  if (req.url === "/api/users" && req.method === "GET") {
+  if (req.url === "/" && req.method === "GET") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(`
+      <html>
+        <head>
+          <title>My Node Server</title>
+        </head>
+        <body>
+          <h1>Hello from Node.js!</h1>
+          <p>My server is working successfully.</p>
+          <p>This page is being served from port 2000.</p>
+        </body>
+      </html>
+    `);
+  }
+  else if (req.url === "/api/users" && req.method === "GET") {
     res.end(JSON.stringify(getUsers()));
   } else if (req.url === "/api/users" && req.method === "POST") {
-    res.end(JSON.stringify({ msg: "add user" }));
+    let body = "";
+    req.on('data', (chunk) => {
+      body += chunk;
+    });
+    req.on("end", () => {
+      const user = JSON.parse(body);
+      const userCreated = addUser(user);
+      res.end(JSON.stringify({ msg: "user added", userCreated }));
+    });
+
   } else if (req.url === "/api/users/2" && req.method === "GET")
     res.end(JSON.stringify({ msg: "single user with id 1" }));
   else if (req.url === "/api/users/1" && req.method == "PUT") {
@@ -12,11 +36,10 @@ const server = http.createServer((req, res) => {
   } else if (req.url === "/api/users/1" && req.method == "DELETE") {
     res.end(JSON.stringify({ msg: "remove 1" }));
   } else {
-    res.statusCode = 404;
+    // res.statusCode = 404;
     res.end();
   }
 });
-
 server.listen(2000, () => {
   console.log("Server is running on port 2000");
 });
